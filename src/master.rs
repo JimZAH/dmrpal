01d6ac4f8e4c4831a4d4da24f4f9d9e4ccee190b
Jim <james@colderwood.com>
2022-09-15 20:01:21 +0100

    Start tracking sessions

--- src/master.rs
+++ src/master.rs
@@ -1,3 +1,23 @@
+use std::time::SystemTime;
+
+pub struct State{
+    talkgroup: u32,
+    sl: u32,
+    stream: u32,
+    timestamp: SystemTime,
+}
+
+impl State{
+    fn default() -> Self {
+        Self {
+            talkgroup: 0,
+            sl: 1,
+            stream: 0,
+            timestamp: std::time::SystemTime::now(),
+        }
+    }
+}
+
 pub fn test_master() {
     println!("testing");
 }
